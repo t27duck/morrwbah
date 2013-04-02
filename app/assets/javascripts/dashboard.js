571da70594282f78c9f935c4ae1f4d38e1d68cda
Tony Drake <t27duck@gmail.com>
2013-04-02 19:35:04 -0400

    Moved JS functions to own file. Link icon in entry bar

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -15,30 +15,6 @@ $(document).ready(function() {
     selectNextEntry('prev');
   });
 
-  function selectNextEntry(direction) {
-     var $selected_entry = $('#feed-entry-list .entry.selected');
-     var $next_selected;
-     if ($selected_entry.size() > 0) {
-       if (direction === 'prev') {
-         $next_selected = $selected_entry.prev();
-       } else {
-         $next_selected = $selected_entry.next();
-       }
-     } else {
-       if (direction === 'prev') {
-         $next_selected = $('#feed-entry-list .entry').last();
-       } else {
-         $next_selected = $('#feed-entry-list .entry').first();
-       }
-     }
-
-     if ($next_selected.size() === 0) {
-       return;
-     }
-     $('.title', $next_selected).trigger('click');
-  }
-
-
   // A feed is selected
   $('#feed-list').on('click', '.feed-title', function() {
     var $entry_list = $('#entry-list');
@@ -142,6 +118,13 @@ $(document).ready(function() {
     data = {"starred": action};
     updateEntry($entry, data);
   });
+  
+  $('#feed-entry-list').on('click', '.entry-link', function() {
+    var $entry = $(this).parent().parent();
+    if ($entry.hasClass('unread')) {
+      $('.read-status', $entry).trigger('click');
+    }
+  });
 
   $('#feed-entry-list').on('click', '#fetch-feed', function() {
     var id = $(this).parent().parent().data('id');
@@ -150,16 +133,6 @@ $(document).ready(function() {
     populateEntryList(id, feed_view, type);
   });
 
-  function updateEntry($entry, data) {
-    var params = {'_method': 'put', 'entry': data};
-    var feed_id = $entry.data('feed-id');
-    var entry_id = $entry.data('id');
-    $.ajax({
-      url: '/feeds/'+feed_id+'/entries/'+entry_id, 
-      type: 'POST', 
-      data: params
-    }).done(function() { });
-  }
 
   $('#feed-entry-list').on('change', '#feed-view', function() {
     var id = $(this).parent().parent().data('id');
@@ -168,101 +141,12 @@ $(document).ready(function() {
     populateEntryList(id, feed_view, type);
   });
 
-  function populateEntryList(id, feed_view, type) {
-    if (typeof feed_view === 'undefined') {
-      feed_view = 'unread';
-    }  
-    $.ajax({
-      url: type+'s/'+id, 
-      data: {feed_view: feed_view},
-      type: 'GET',
-      beforeSend: function() {
-        $('#loading-notice.notice').removeClass('hidden');
-      }
-    }).done(function(data) {
-      $('#feed-entry-list').html(data);
-      refreashFeedList(type, id);
-      $('#loading-notice.notice').addClass('hidden');
-      $(window).trigger('resize');
-    });
-  }
-
-  function refreashFeedList(type, active_id) {
-    $.get('feeds', function(data) {
-      $('#feed-list').html(data);
-      $('#feed-list .feed-title div').removeClass('selected');
-      $('#feed-list .folder-title div').removeClass('selected');
-      if (typeof active_id != 'undefined') {
-        $("."+type+"-title[data-id='"+active_id+"']").children('div').addClass('selected');
-      }
-      setSortableOnFeedList();
-    });
-  }
-
   $(window).resize(function() {
     if ($('#entry-list').size() > 0) {
       $('#feed-list').height($(window).height() - $('#feed-list').offset().top - 50);
       $('#entry-list').height($(window).height() - $('#entry-list').offset().top - 15);
     }
   });
-
-  function setSortableOnFeedList() {
-    $('.sortable', '#feed-list').nestedSortable({
-      forcePlaceholderSize: true,
-      handle: 'div',
-      items: 'li',
-      opacity: .6,
-      placeholder: 'placeholder',
-      tabSize: 0,
-      toleranceElement: '> div',
-      maxLevels: 2,
-      isTree: true,
-      protectRoot: true,
-      axis: 'y',
-      update: function(event, ui) {
-        var folder_structure_data = $(this).nestedSortable('toHierarchy');
-        $.ajax({
-          type: "POST",
-          url: "/folders/update_order.json",
-          data: JSON.stringify({folder_structure: folder_structure_data}),
-          contentType: "application/json; charset=utf-8",
-          dataType: "json",
-        }).done(function(data, textStatus, jqXHR) {
-        }).fail(function(jqXHR, textStatus, errorThrown) {
-        });
-      }
-    });
-  }
-
-  function updateUnreadCount(feed_id, direction) {
-    var $feed_title = $('#feed-'+feed_id+'.feed-title');
-    var $folder_title = $feed_title.closest('li.folder-title');
-    var $all_items = $('#all-items.feed-title');
-    adjustUnreadCountOnElement($feed_title, direction);
-    adjustUnreadCountOnElement($folder_title, direction);
-    adjustUnreadCountOnElement($all_items, direction);
-  }
-
-  function adjustUnreadCountOnElement($elm, direction) {
-    var current_unread = $elm.data('unread-count');
-    var $div_handle = $('div:first', $elm);
-    var $unread_count_span = $('span.unread-count:first', $elm);
-    var new_unread;
-    if (direction === 'up') {
-      new_unread = current_unread + 1;
-    } else {
-      new_unread = current_unread - 1;
-    }
-
-    $elm.data('unread-count', new_unread);
-    if (new_unread > 0) {
-      $div_handle.addClass('unread');
-      $unread_count_span.html('('+new_unread+')');
-    } else {
-      $div_handle.removeClass('unread');
-      $unread_count_span.html('');
-    }
-  }
 
   setSortableOnFeedList();
   $(window).trigger('resize');
--- db/migrate/20130319011410_create_feeds.rb
+++ db/migrate/20130319011410_create_feeds.rb
@@ -9,6 +9,7 @@ class CreateFeeds < ActiveRecord::Migration
       t.string :feed_url, :null => false
       t.string :etag
       t.string :icon_path
+      t.boolean :sanitize, :null => false, :default => true
       t.datetime :last_modified, :null => false
       t.datetime :last_checked
 
